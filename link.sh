24bf85179014463965f7dfd43ae6ffbfc7e0f445
Jay Freestone <mail@jayfreestone.com>
2016-03-30 16:46:37 +0100

    Adds tern project file

--- link.sh
+++ link.sh
@@ -7,7 +7,7 @@
 # Variables
 
 dir=~/dotfiles # dotfiles directory
-files="bash_profile agignore eslintrc gitignore scss-lint.yml ctags" # files to symlink
+files="bash_profile agignore eslintrc gitignore scss-lint.yml ctags tern-project" # files to symlink
 
 # Symlink Creation
 
