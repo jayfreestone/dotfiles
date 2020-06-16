8024222fcce2940290117f7c5c1dc81497e055dc
Jay Freestone <mail@jayfreestone.com>
2020-06-16 10:56:12 +0100

    Update FZF git integration.

--- zsh/aliases.inc.zsh
+++ zsh/aliases.inc.zsh
@@ -2,6 +2,11 @@
 alias ls='ls -Glah'
 alias mkdir='mkdir -pv'
 alias grd='cd $(git rev-parse --show-toplevel)'
+alias gfd='git rev-list -n 1 HEAD --' # find the commit that deleted a file
+alias fza='git ls-files -m -o --exclude-standard | fzf --print0 -m | xargs -0 -t -o git add'
+
+# macOS specific
+alias y="open -a Yoink"
 
 # Docker
 alias dc='docker-compose'
