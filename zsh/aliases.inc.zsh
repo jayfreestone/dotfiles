a7232eaf1c2d05beeb109e873d9514287682f953
Jay Freestone <mail@jayfreestone.com>
2020-08-04 17:44:46 +0100

    Add most commits alias.

--- zsh/aliases.inc.zsh
+++ zsh/aliases.inc.zsh
@@ -1,9 +1,12 @@
-# Aliases
+# Generic
 alias ls='ls -Glah'
 alias mkdir='mkdir -pv'
+
+# Git
 alias grd='cd $(git rev-parse --show-toplevel)'
 alias gfd='git rev-list -n 1 HEAD --' # find the commit that deleted a file
 alias fza='git ls-files -m -o --exclude-standard | fzf --print0 -m | xargs -0 -t -o git add'
+alias mostcommits='git shortlog -sn --all --no-merges'
 
 # macOS specific
 alias y="open -a Yoink"
