c6c3ebe98fe507ff2634dacb550a6f7cfff2e0fe
Jay Freestone <mail@jayfreestone.com>
2020-08-04 17:44:28 +0100

    Update zettel helper.

--- zsh/functions/zet.inc.zsh
+++ zsh/functions/zet.inc.zsh
@@ -1,4 +1,20 @@
+#!/usr/bin/env zsh
+
+readonly ZETTEL_DIR=~/zettelkasten/zettelkasten.wiki
+
 # Create new zettelkasten note
+# Example:
+#   zet My Great Note
 zet() {
-  nvim "+Zet $*"
+  local title=$@
+  # Lowercase it and replace spaces with dashes
+  local slugified_title=$(echo ${title:l} | sed 's/ /-/g')
+  local note_name="$(date +"%Y%m%d%k%M")-$slugified_title"
+  local note_body="ID: $note_name\nTags:\n\n# $title"
+  local note_location="$ZETTEL_DIR/$note_name.md"
+
+  echo $note_body > $note_location
+
+  # Open it in IntelliJ
+  idea $note_location
 }
